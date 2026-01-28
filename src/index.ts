1f0bc21f4ed4d8423f2d5ee16e3e93d16a7b4853
yizhe-ang <ang.yizhe@u.nus.edu>
2026-01-28 14:21:47 +0800

    expose SyncMSDFText

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { MSDFText } from "./MSDFText"
-import { MSDFTextGeometry } from "./MSDFTextGeometry"
-import { MSDFTextNodeMaterial } from "./MSDFTextMaterial"
+import { MSDFText, SyncMSDFText } from "./MSDFText";
+import { MSDFTextGeometry } from "./MSDFTextGeometry";
+import { MSDFTextNodeMaterial } from "./MSDFTextMaterial";
 
 export { type BMFontJSON } from "@/types/bmfont-json";
-export { MSDFText, MSDFTextGeometry, MSDFTextNodeMaterial  } 
+export { MSDFText, MSDFTextGeometry, MSDFTextNodeMaterial };
